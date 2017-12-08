647a6f4f09d0991c7a357605fa788c55f88f4ebb
RainingNight <leshin@live.com>
2017-12-08 23:37:06 +0800

    refactor struct

---
+++ test/Zero.Logging.Tests/BigDataTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Zero.Logging.Tests
+{
+    public class BigDataTest
+    {
+        private readonly ITestOutputHelper output;
+        public BigDataTest(ITestOutputHelper outputHelper)
+        {
+            output = outputHelper;
+        }
+
+        /// <summary>
+        /// 使用Nlog连续插入20W行字符串
+        /// </summary>
+        [Fact]
+        public void NlogTest()
+        {
+            NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+            var total = 200000;
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < total; i++)
+            {
+                log.Info("nlog bigdata test: " + i);
+            }
+            sw.Stop();
+            log.Info($"total: {total}, Elapsed:{sw.ElapsedMilliseconds}");
+            output.WriteLine($"NLog测试 total: {total}, Elapsed:{sw.ElapsedMilliseconds}");
+        }
+    }
+}
